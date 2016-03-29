5e11ee21f2b7eee9e652fffa9ca108a3da31abf1
superandrew123 <andrewmiksch@gmail.com>
2016-03-28 21:01:24 -0400

    basics done

--- guy.js
+++ guy.js
@@ -1,19 +1,22 @@
-var places = ["place1","place2","place3","place4","place5"];
-var foodStyles = ["foodStyle1","foodStyle2","foodStyle3","foodStyle4","foodStyle5"];
-var names = ["name1","name2","name3","name4","name5"];
-var foodTypes = ["foodType1","foodType2","foodType3","foodType4","foodType5"];
-var restaurantTypes = ["restaurantType1","restaurantType2","restaurantType3","restaurantType4","restaurantType5"];
+function makeRestauraunt(){
+  var places = ["place1","place2","place3","place4","place5"];
+  var foodStyles = ["foodStyle1","foodStyle2","foodStyle3","foodStyle4","foodStyle5"];
+  var names = ["name1","name2","name3","name4","name5"];
+  var foodTypes = ["foodType1","foodType2","foodType3","foodType4","foodType5"];
+  var restaurantTypes = ["restaurantType1","restaurantType2","restaurantType3","restaurantType4","restaurantType5"];
 
-var title = [];
-title.push(returnName(places));
-title.push(returnName(foodStyles));
-title.push(returnName(names));
-title.push(returnName(foodTypes));
-title.push(returnName(restaurantTypes));
+  var title = [];
+  title.push(returnName(places));
+  title.push(returnName(foodStyles));
+  title.push(returnName(names));
+  title.push(returnName(foodTypes));
+  title.push(returnName(restaurantTypes));
 
-document.getElementById("name-holder").innerHtml(title.join(" "));
+  document.getElementById("name-holder").innerHTML = title.join(" ");
 
-function returnName(data){
-  var index = Math.floor(Math.random() * data.length);
-  return data[index];
+  function returnName(data){
+    var index = Math.floor(Math.random() * data.length);
+    return data[index];
+  }
 }
+// makeRestauraunt();
